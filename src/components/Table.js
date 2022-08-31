8f5d9550cc7f894e8863627cb1f32afbce8b0730
Robert Santos <robertdosantos09@gmail.com>
2022-08-31 19:34:32 -0300

    Resolução do 7° requisito

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,25 +1,77 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Table extends Component {
+  valorMoeda = (obj, condicao, chave) => {
+    let res = Object.entries(obj).find((e) => e[0] === condicao);
+    res = res[1][chave];
+    return res;
+  };
+
+  linhaTabela = () => {
+    const { expenses } = this.props;
+    return expenses.map((e) => ([
+      e.id,
+      e.description,
+      e.tag,
+      e.method,
+      Number(e.value).toFixed(2),
+      this.valorMoeda(e.exchangeRates, e.currency, 'name'),
+      Number(this.valorMoeda(e.exchangeRates, e.currency, 'ask')).toFixed(2),
+      (Number(this.valorMoeda(e.exchangeRates, e.currency, 'ask'))
+      * Number(e.value)).toFixed(2),
+      'Real',
+    ]));
+  };
+
   render() {
+    const rowTable = this.linhaTabela();
+
     return (
       <div>
         <table>
-          <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
-          </tr>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              rowTable.map((e) => (
+                <tr key={ e[0] }>
+                  <td>{ e[1] }</td>
+                  <td>{ e[2] }</td>
+                  <td>{ e[3] }</td>
+                  <td>{ e[4] }</td>
+                  <td>{ e[5] }</td>
+                  <td>{ e[6] }</td>
+                  <td>{ e[7] }</td>
+                  <td>{ e[8] }</td>
+                </tr>
+              ))
+            }
+          </tbody>
         </table>
       </div>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
